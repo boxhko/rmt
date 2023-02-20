a344324b85d682164d60a31a82d616bdab42e99e
Александр Божко <125882660+boxhko@users.noreply.github.com>
2023-02-20 16:51:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+int num;
+
+Console.WriteLine("Введите число: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (num%5==0 && num%3==0)
+{
+    Console.WriteLine("fizzbuzz");
+}
+
+
+
+else
+{
+    if (num%5==0)
+    {
+        Console.WriteLine("buzz");
+    }
+    if (num%3==0)
+    {
+        Console.WriteLine("fizz");
+    }
+}
